669f8d20abf868c1942247924c83a8a5c7f8e327
Sillyplus <oi_boy@sina.cn>
2015-09-05 04:36:03 +0800

    bug fix

--- Timus/1033.cpp
+++ Timus/1033.cpp
@@ -25,30 +25,28 @@ queue<pair<int, int> > Q;
 
 int main() {
     int n;
-    scanf("%d", &n);
+    cin >> n;
     for (int i = 0; i < n; i++)
-        scanf("%s", &mp[i]);
+        cin >> mp[i];
     Q.push(make_pair(0, 0));
     Q.push(make_pair(n-1, n-1));
-    vis[0][0] = true;
-    vis[n-1][n-1] = true;
     int ans = 0;
     while (!Q.empty()) {
         pair<int, int> src;
         src = Q.front();
         Q.pop();
+        if (vis[src.first][src.second])
+            continue;
         for (int i = 0; i < 4; i++) {
             int tx = src.first + dx[i];
             int ty = src.second + dy[i];
-            if (!vis[tx][ty]) {
-                if ((tx < 0 || tx >= n || ty < 0 || ty >= n) || mp[tx][ty] == '#') {
-                    ans++;
-                } else if (mp[tx][ty] == '.') {
-                    vis[tx][ty] = true;
-                    Q.push(make_pair(tx, ty));
-                }
+            if ((tx < 0 || tx >= n || ty < 0 || ty >= n) || mp[tx][ty] == '#') {
+                ans++;
+            } else if (mp[tx][ty] == '.') {
+                Q.push(make_pair(tx, ty));
             }
         }
+        vis[src.first][src.second] = true;
     }
     ans -= 4;
     cout << ans * 9 << endl;
